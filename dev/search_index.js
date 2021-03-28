e31c14b195b242d8d3697459c887015e581df15f
Documenter.jl <documenter@juliadocs.github.io>
2021-03-28 19:57:13 +0000

    build based on 25bf5f1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PortfolioBedtest","category":"page"},{"location":"#PortfolioBedtest","page":"Home","title":"PortfolioBedtest","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PortfolioBedtest.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PortfolioBedtest]","category":"page"}]
+}
